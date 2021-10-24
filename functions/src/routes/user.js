8295a2d9c3cd0614980b3013c540ad38a8a24e6e
Prajal Patidar <prajalpatidar01@gmail.com>
2021-10-25 01:57:01 +0530

    .

--- functions/src/routes/user.js
+++ functions/src/routes/user.js
@@ -43,7 +43,7 @@ route.post("/signup", (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         rating: 0,
-        imageUrl:`https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/noImg.png?alt=media`,
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/noImg.png?alt=media`,
         createdAt: new Date().toISOString(),
         userId,
       };
@@ -94,36 +94,35 @@ route.get("/:handle", (req, res) => {
   db.doc(`/users/${req.params.handle}`)
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        userData = doc.data();
-        return db
-          .collection("screams")
-          .where("handle", "==", req.params.handle)
-          .get();
-      } else {
-        return res.status(400).json({ error: "user not found" });
-      }
+        if (doc.exists){
+          userData = doc.data();
+          return db
+            .collection("screams")
+            .where("handle", "==", req.params.handle)
+            .get();
+        } 
+        else return res.status(400).json({ error: "user not found" });
     })
     .then((data) => {
-      userData.screams = [];
-      data.forEach((doc) => {
-        userData.screams.push({
-          title: doc.data().title,
-          userImage: doc.data().imageUrl,
-          body: doc.data().body,
-          createdAt: doc.data().createdAt,
-          handle: doc.data().authorHandle,
-          rating: doc.data().rating,
-          requireSkills: doc.data().requireSkills,
-          url: doc.data().url,
-          screamId: doc.id,
+        userData.screams = [];
+        data.forEach((doc) => {
+          userData.screams.push({
+            title: doc.data().title,
+            userImage: doc.data().imageUrl,
+            body: doc.data().body,
+            createdAt: doc.data().createdAt,
+            handle: doc.data().authorHandle,
+            rating: doc.data().rating,
+            requireSkills: doc.data().requireSkills,
+            url: doc.data().url,
+            screamId: doc.id,
+          });
         });
-      });
-      return res.json(userData);
+        return res.json(userData);
     })
-    .then((err) => {
+    .catch(err => {
       console.error(err);
-      return res.status(500).json({ error: err.code });
+      return res.status(500).json({error: err.code});
     });
 });
 
@@ -149,41 +148,45 @@ route.post("/image", FBAuth, (req, res) => {
   const fs = require("fs");
 
   const busboy = new BusBoy({ headers: req.headers });
-    let imageFileName;
-    let imageToBeUploaded;
+  let imageFileName;
+  let imageToBeUploaded;
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-      if(mimetype !== 'image/png' && mimetype !== 'image/jpeg')
-        return res.status(400).json({error:"wrong file type submitted"})
-      const imageExtension = filename.split('.')[filename.split('.').length - 1]
-      imageFileName = `${Math.round(Math.random()*100000000000)}.${imageExtension}`
-      const filePath = path.join(os.tmpdir(),imageFileName)
-      imageToBeUploaded = {filePath , mimetype}
-      file.pipe(fs.createWriteStream(filePath))
+    if (mimetype !== "image/png" && mimetype !== "image/jpeg")
+      return res.status(400).json({ error: "wrong file type submitted" });
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 100000000000
+    )}.${imageExtension}`;
+    const filePath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filePath, mimetype };
+    file.pipe(fs.createWriteStream(filePath));
   });
 
-  busboy.on('finish',()=>{
-      admin.storage().bucket().upload(imageToBeUploaded.filePath,{
-          resumable: false,
-          metadata:{
-              metadata:{
-                  contentType:imageToBeUploaded.mimetype
-              }
-          }
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filePath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+          },
+        },
       })
-      .then(()=>{
-          const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
-          return db.doc(`/users/${req.user.handle}`).update({imageUrl})
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
       })
-      .then(()=>{
-          return res.json({message:'Image uploaded successfully'})
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
       })
-      .catch(err =>{
-          console.log(err)
-          return res.status(500).json({error: err.code})
-      })
-  })
-  busboy.end(req.rawBody)
-
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 });
 
 // get Author details
